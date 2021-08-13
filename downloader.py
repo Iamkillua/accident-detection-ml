89416d023e9b3016191f46ca31edb64e5942c022
Harshit <harshdeep.tomar@gmail.com>
2021-08-13 21:28:15 +0530

    added extraction support

--- downloader.py
+++ downloader.py
@@ -2,5 +2,6 @@ import os
 
 os.system("aria2c " + "https://mediacenter.harshdeepsfiles.workers.dev/0:/accident-detection.rar")
 				
-
+import patoolib
+patoolib.extract_archive("accident-detection.rar", outdir=".")
 
