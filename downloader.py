29eab56db8c2928280bb6492710ec1376f6ee8c8
Harshit <harshdeep.tomar@gmail.com>
2021-08-13 21:27:03 +0530

    added download url

--- downloader.py
+++ downloader.py
@@ -1,38 +1,6 @@
-import requests
+import os
 
-def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)    
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
+os.system("aria2c " + "https://mediacenter.harshdeepsfiles.workers.dev/0:/accident-detection.rar")
 				
 
-file_id = '1gKCf19_Mn5YSAE6ofZ3xIUhKjlkTKe7N'
-destination = 'accident-detection.rar'
-download_file_from_google_drive(file_id, destination)
 
-import patoolib
-patoolib.extract_archive("accident-detection.rar", outdir=".")
